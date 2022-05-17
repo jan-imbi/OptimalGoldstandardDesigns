d8a870172085aa186d4c1acef1b3b7035b2b9463
Jan Meis <meis@imbi.uni-heidelberg.de>
2022-05-17 09:12:11 +0200

    new sticker

--- sticker/sticker.R
+++ sticker/sticker.R
@@ -1,12 +1,12 @@
 library(hexSticker)
 library(ggplot2)
-imgurl <- here::here("misc/screenshot.png")
+imgurl <- here::here("sticker/screenshot.tiff")
 sticker <- sticker(imgurl,
-                   package = "OptimalGoldstandardDesigns", p_size = 8 * 4000 / 300,
+                   package = "OptimalGoldstandardDesigns", p_size = 8 * 6000 / 300,
                    s_x = 1, s_y = .85, s_width = .85,
                    p_color = "black",
                    h_fill = "white",
                    h_color = "#cdcdff",
                    filename = "man/figures/sticker.png",
-                   dpi = 4000
+                   dpi = 6000
 )
